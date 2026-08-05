40f900cc415414461222aa773ae54663de509730
calcasey <chloe-casey@hotmail.co.uk>
2026-08-05 19:39:24 +0200

    Add session 4

---
+++ session_4/variant_calling_examples_cemm/example_scripts/01_fastp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=fastp
+#SBATCH --output=logs/slurm_logs/fastp_%j.out
+#SBATCH --error=logs/slurm_logs/fastp_%j.err
+#SBATCH --time=00:05:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=3G
+#SBATCH --partition=tinyq
+#SBATCH --qos=tinyq
+
+set -euo pipefail # Exit on error, undefined variable, or error in a pipeline
+
+module load fastp/0.23.2-GCC-11.3.0
+
+SAMPLE="SRR7890883"
+IN_DIR="../../example_data/fastq"
+OUT_DIR="results/01_fastp"
+LOG_DIR="logs/01_fastp/"
+
+mkdir -p "${OUT_DIR}"
+mkdir -p "${LOG_DIR}"
+
+RAW_R1="${IN_DIR}/${SAMPLE}.chr17_50k_R1.fastq"
+RAW_R2="${IN_DIR}/${SAMPLE}.chr17_50k_R2.fastq"
+TRIMMED_R1="${OUT_DIR}/${SAMPLE}.chr17_50k_R1_trimmed.fastq"
+TRIMMED_R2="${OUT_DIR}/${SAMPLE}.chr17_50k_R2_trimmed.fastq"
+FASTP_HTML="${OUT_DIR}/${SAMPLE}.chr17_50k.html"
+FASTP_JSON="${OUT_DIR}/${SAMPLE}.chr17_50k.json"
+LOG="${LOG_DIR}/${SAMPLE}.fastp.log"
+
+echo "Running fastp on ${SAMPLE}"
+fastp \
+    --in1 "${RAW_R1}" \
+    --in2 "${RAW_R2}" \
+    --out1 "${TRIMMED_R1}" \
+    --out2 "${TRIMMED_R2}" \
+    --detect_adapter_for_pe \
+    --thread 1 \
+    --html "${FASTP_HTML}" \
+    --json "${FASTP_JSON}" > "${LOG}" 2>&1
